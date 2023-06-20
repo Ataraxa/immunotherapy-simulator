b96f4f991a802f3a87638256decdaf765869572f
Alexandre Péré <93291204+Ataraxa@users.noreply.github.com>
2023-06-20 11:16:23 +0200

    started vectorising fitness function

--- Vectorised Env/fitness_vectorised.m
+++ Vectorised Env/fitness_vectorised.m
@@ -1,34 +1,30 @@
-function error = fitness_function(params_array)
-
+function error = fitness_vectorised(params_array)
     % 21 parameters
-    params_list = ["td", "t_delay", "t_last", "t_delay12", "t_last12", ...
-        "k1", "k2", "k3", "k4", "k5", "k6", ...
-        "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", ...
-        "s1", "s2"];
+    params_list = [
+                   "td", "t_delay", "t_last", "t_delay12", "t_last12", ...
+                   "k1", "k2", "k3", "k4", "k5", "k6",                 ...
+                   "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",     ...
+                   "s1", "s2"
+                   ];
 
     load Data/preprocessed_data data
 
     for i = 1:numel(params_list)
         p.(params_list(i)) = params_array(i);
     end
-    disp(p)
-    
-    treatments_available = methods(treatment_factory);
+
+    treatments_available = methods(treatment_factory)
     treatment_processed = 1;
-    error_per_treatment = [];
-
-
-
     % Iterate over all treatments
+%     disp(p) % To control passing of parameters
+    error_per_treatment = [];
     for treatment = treatments_available(1:end-1).' % Need to remove last method bc it's not a real methods (it's a callback)
-        tic;
         treatment_spec = treatment_factory.(treatment{1})();
         plot_info.flag = false; plot_info.treatment_name = treatment{1};
         plot_info.subplot_id = treatment_processed;
         sol = immuno_solver(p, treatment_spec, plot_info);
-        toc
 
-        tic;
+%         fprintf("Error Array for %s \n ", treatment{1})
         % Calculate MSE between experiment data and solution for each
         % treatment and rake average of average for overall error
         i = 1;
@@ -36,6 +32,7 @@ function error = fitness_function(params_array)
         for day = data.(treatment{1}).valid_days
             in_vivo = data.(treatment{1}).vector_avg(i);
             [~, sol_index] = min(abs(day .* ones(1, numel(sol.x)) - sol.x));
+%             disp(sol_index)
 %             figure(1); subplot(2, 3, treatment_processed); hold on
             total_tumour_vol = sol.y(4,:) + sol.y(5,:);
 %             scatter(sol.x(sol_index), total_tumour_vol(sol_index), "b", "HandleVisibility","off")
@@ -43,12 +40,13 @@ function error = fitness_function(params_array)
             error_per_day(end+1) = ((in_silico - in_vivo)^2)/1000;
             i = i + 1;
         end
-        toc
+
+%     disp(error_per_day)
+%     fprintf("END OF TREATMENT ANALYSIS \n ")
     error_per_treatment(treatment_processed) = mean(error_per_day);
     treatment_processed = treatment_processed + 1;
     end
 error = mean(error_per_treatment);
 disp(error)
-toc
 end
 
--- fitness_test.m
+++ fitness_test.m
@@ -1,7 +1,9 @@
-clc; clear; close all;
+clear; close all;
 
 load Parameters\struct_christian.mat params;
 vectorised_params = cell2mat(struct2cell(params));
-fitness_function(vectorised_params)
-set(gcf, 'Position', get(0, 'Screensize'));
+tic;
+fitness_function(vectorised_params);
+toc
+% set(gcf, 'Position', get(0, 'Screensize'));
 fprintf("Test run successfully\n")
