572ea8de0bb11cb0b70129e27ca0b19496a9e5f9
Alexandre Péré <93291204+Ataraxa@users.noreply.github.com>
2023-10-10 10:00:55 +0200

    detected a bug in anarchical model - solver behaves badly with ForwardDiff.Dual - possible fix

--- Partial Pooling BPE/Core/ga_optimiser.jl
+++ Partial Pooling BPE/Core/ga_optimiser.jl
@@ -2,13 +2,13 @@ using Evolutionary
 using JLD2
 using DifferentialEquations
 
-include("../Model/ode_model.jl")
+include("../Model/Differential/ode_model.jl")
 include("../Library/optimisation_lib.jl")
 
 # Fetch known fit parameters for boxConstraints
 u0, p = get_default_values() # u0 has 4 params, p has 21 params
 target = [p; u0[1:end-1]] # 1x25 vector
-width_factor = 0.1
+width_factor = 0.25
 lower = (1 - width_factor) .* target  # Add unknown params at the end
 upper = (1 + width_factor) .* target
 
@@ -24,8 +24,8 @@ params = Evolutionary.optimize(
     ),
     Evolutionary.Options(
         parallelization=:thread,
-        iterations = 10000,
-        successive_f_tol=2000
+        iterations = 10_000,
+        successive_f_tol=20_000
     )
 )
 
--- Partial Pooling BPE/Model/Bayesian/anarchical.jl
+++ Partial Pooling BPE/Model/Bayesian/anarchical.jl
@@ -13,6 +13,9 @@ so that only the first _n_ rows are used.
         std_k6, std_d1, std_s2,
         max1, max2,
         exp_err)
+    
+    printn("__________________________________________________")
+
     # Regular priors
     k6 ~ truncated(Normal(-0.7, std_k6); lower=-100, upper=log(max1))
     d1 ~ truncated(Normal( 2.3, std_d1); lower=-100, upper=log(max2))
@@ -23,15 +26,21 @@ so that only the first _n_ rows are used.
 
     # Simulate model
     p = [exp(k6), exp(d1), exp(s2)]
+    println(p)
+
+    # Experimental block 
+    for param in p 
+        if typeof(param) == Dual{ForwardDiff.Tag{Turing.TuringTag, Float64}, Float64, 3}
+            param = param.value 
+        end 
+    end
 
     predictions = solve(problem; p=p, saveat=0.1)
     pred_vol = predictions[4,:] + predictions[5,:]
     sliced_pred = pred_vol[selected_days*trunc(Int, 1/s) .+ 1]
-
+    println(p)
+    println(sliced_pred)
     for i in eachindex(sliced_pred)
-        println("__________________________________________________")
-        println(p)
-        println(sliced_pred[i])
         data[i] ~ Normal(log(sliced_pred[i]), σ_err)
     end
 end
