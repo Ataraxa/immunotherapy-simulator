a45a342d8de3628f3c46f9ae78c3862b9cb3672d
Alexandre Péré <93291204+Ataraxa@users.noreply.github.com>
2023-12-17 19:24:48 +0100

    added interim report template

--- JuliaLang/Tools/visualisation/mcmc_visualiser.jl
+++ JuliaLang/Tools/visualisation/mcmc_visualiser.jl
@@ -7,15 +7,7 @@ using MCMCChains
 using MCMCChainsStorage
 using StatsPlots: plot, savefig
 
-chain = h5open("Results/hpc-individual-10￼
-Home
-Questions
-Tags
-Users
-Unanswered
-TEAMS
-Stack Overflow for Teams – Start collaborating and sharing organizational knowledge.￼Create a free Team Why Teams?
-Consistency of Posterior-2.h5", "r") do f
+chain = h5open("Results/hpc-individual-10-1.h5", "r") do f
 # chain = h5open("Results/restr3_n10_log.h5", "r") do f
     read(f, Chains)
 end
@@ -23,10 +15,10 @@ a = namesingroup(chain, :k6)
 plotd = plot(chain[:,:,[1,2,3,4,5]])
 # plotd = plot(chain[:,:,:])
 display(plotd)
-savefig(plotd,"Misc/Images/batch2/takuya_restr12.png")
+# savefig(plotd,"Misc/Images/batch2/takuya_restr12.png")
 
 # # Gelman diagnostic: PSRFCI under 1.1 indicates good mixing and convergence
-display(local df_gelman = gelmandiag(chain[:,:,[3,4,5]]))
+display(local df_gelman = gelmandiag(chain[:,:,[1,2,3,4,5]]))
 
 # # # Autocorrelation
 # display(autocorplot(chain))
--- JuliaLang/Tools/visualisation/visualise_fake_data.jl
+++ JuliaLang/Tools/visualisation/visualise_fake_data.jl
@@ -1,5 +1,6 @@
-using Plots: plot, plot!, vline!, scatter!
+using Plots: plot, plot!, vline!, scatter!, savefig
 using DelimitedFiles: readdlm
+using LaTeXStrings
 
 include("../../Model/Differential/ode_core.jl")
 include("../../Model/Differential/ode_params.jl")
@@ -12,6 +13,8 @@ data = readdlm("Data/fakeData/trajectories-$(data_set).csv", ',')
 ### Visualise raw traces 
 # scatter(0:0.1:27, data[]')
 
+myplot = plot(ylabel=L"Tumour volume ($mm^3$)", xlabel="Days since tumour inoculation")
+
 ### Visualise baseline 
 params = exp.([-0.0852590858609503, 1.189102819703836, -0.3274023044544697])
 rep, _ = repack_params(updateParams3(params...))
@@ -19,13 +22,15 @@ base_pb = create_problem()
 
 sol = solve(base_pb; p=rep, saveat=0.1)
 pred = sol[4,:] + sol[5,:]
-plot(0:0.1:27, pred)
+plot!(0:0.1:27, pred, linestyle=:dash, lw=2.5, seriescolor=:black)
 
 ### Visualise experimental (noisy + sliced)
 
 selected_days = [0,7,8,9,11,14,17,20]
+max=5
 # my_plot = scatter(selected_days, data[1:5,:]')
 # vline!([0,7,8,9,11,14,17,20]; alpha=0.5, legend=false)
-plot!(selected_days, data[1:5, selected_days*trunc(Int, 1/0.1) .+ 1]'; opacity=0.5)
-scatter!(selected_days, data[1:5, selected_days*trunc(Int, 1/0.1) .+ 1]')
-# display(my_plot)
+plot!(myplot, selected_days, data[1:max, selected_days*trunc(Int, 1/0.1) .+ 1]'; opacity=0.5)
+scatter!(myplot, selected_days, data[1:max, selected_days*trunc(Int, 1/0.1) .+ 1]'; legend=false)
+
+savefig(myplot, "vis_fd.png")
